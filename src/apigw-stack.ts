fd2d8c070a697d25f4c720a99dcdf1f2ab38881e
martin.mueller <m.mueller@unimed.de>
2020-12-31 13:08:51 +0100

    feat: put stack in its own file

---
+++ src/apigw-stack.ts
@@ -0,0 +1,17 @@
+import * as apigw from '@aws-cdk/aws-apigateway';
+import * as core from '@aws-cdk/core';
+import { openApi } from './openapi';
+import { StaticSite } from './static-site';
+
+export class ApiGwStack extends core.Stack {
+  constructor(scope: core.Construct, id: string, props: core.StackProps = {}) {
+    super(scope, id, props);
+
+    new apigw.SpecRestApi(this, 'SpecRestApi', {
+      restApiName: 'Petstore Example',
+      apiDefinition: apigw.ApiDefinition.fromInline(openApi),
+    });
+
+    new StaticSite(this);
+  }
+}
