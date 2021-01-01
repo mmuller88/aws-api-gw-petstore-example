edac16bba7b4d0416cd58477541b21cb6351ccea
martin.mueller <m.mueller@unimed.de>
2021-01-01 18:47:38 +0100

    feat: try bucket deploy for stage

--- src/static-site.ts
+++ src/static-site.ts
@@ -12,7 +12,8 @@ export class StaticSite {
   constructor(scope: Stack, props: StaticSiteProps) {
     // eslint-disable-next-line @typescript-eslint/no-require-imports
     const fs = require('fs');
-    fs.writeFileSync('./src/site-contents/openapi.json', JSON.stringify(openApi(props.stage)));
+    fs.writeFileSync('./src/site-contents/openapi-dev.json', JSON.stringify(openApi('dev')));
+    fs.writeFileSync('./src/site-contents/openapi-prod.json', JSON.stringify(openApi('prod')));
     /**
      * NOTE: S3 requires bucket names to be globally unique across accounts so
      * you will need to change the bucketName to something that nobody else is
@@ -40,8 +41,14 @@ export class StaticSite {
 
     // Deploy site contents to S3 bucket
     // tslint:disable-next-line: no-unused-expression
-    new s3deploy.BucketDeployment(scope, 'DeployWithInvalidation', {
+    new s3deploy.BucketDeployment(scope, 'BucketDeployment', {
       sources: [s3deploy.Source.asset('./src/site-contents')],
+      destinationBucket: siteBucket,
+    });
+
+    new s3deploy.BucketDeployment(scope, 'BucketDeploymentOpenApi', {
+      sources: [s3deploy.Source.asset(`./src/site-contents/openapi-${props.stage}.json`)],
+      destinationKeyPrefix: '/src/site-contents/openapi.json',
       destinationBucket: siteBucket,
     });
   }
